aefc97edd074261a13435b4c7af110cf5d91b36c
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-01-13 20:01:20 +0100

    a

--- javaproperties/src/main/java/beans/PropertiesBean.java
+++ javaproperties/src/main/java/beans/PropertiesBean.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package beans;
+
+import javax.ejb.Stateless;
 
 /**
  *
- * @author Zanders andra acc
+ * @author Zander Koch
  */
+@Stateless
 public class PropertiesBean {
     
+    public String getProp(String key){
+        
+    }
 }
